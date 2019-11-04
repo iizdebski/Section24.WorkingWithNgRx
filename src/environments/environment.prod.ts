6ffbbfb06c3587fdad4b6b6ade732bf924c617eb
ivan <iizdepskyy112@gmail.com>
2019-11-04 20:04:19 +0200

    Section 23. Deploying an Angular App. 339-343

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyC-_Xy8tGotQML5m36a8JhIOAbwjC91Q7I'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyC-_Xy8tGotQML5m36a8JhIOAbwjC91Q7I'
 };
 
 /*
